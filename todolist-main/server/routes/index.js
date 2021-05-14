29dad48441ca77cd19b1df7b44012b089e78d351
lhacevedo <81338940+lhacevedo@users.noreply.github.com>
2021-05-14 00:46:55 -0700

    Delete todolist-main directory

--- todolist-main/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-// ./routes/index.js
-const tasks = require('./tasks');
-// const photos = require('./photos');
-
-module.exports = app => {
-  app.use('/api/tasks', tasks);
-  // app.use('/photos', photos);
-  // etc..
-};
